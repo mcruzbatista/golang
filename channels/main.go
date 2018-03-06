bffc4d9874cad989b1b6fbc3d339919b92a3342f
Marcos <marcos.batista@pigpeg.com>
2018-03-06 18:42:49 -0300

    add channel module

---
+++ channels/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main() {
+
+	links := []string{
+		"http://google.com",
+		"http://golang.org",
+		"http://facebook.com",
+	}
+
+	c := make(chan string)
+
+	for _,link := range links {
+		go checkLink(link, c)
+	}
+
+	for {
+		go checkLink(<- c, c)
+	}
+}
+
+func checkLink(link string, c chan string){
+	_,err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- link
+}
